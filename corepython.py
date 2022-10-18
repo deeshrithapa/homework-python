9ee54f28a594ad45aee5ee41f9763230acabc56e
deshrithapa <deeshri.t@gmail.com>
2022-10-18 11:38:02 +0545

    first

---
+++ corepython.py
@@ -0,0 +1,63 @@
+# 1. Write a program which prints “Hello World of Python”.
+# word = input("Hello World of Python.")
+# print(word)
+
+# 2. Write a program which prints your personal information (person id, full name, contact address).
+# name = input("name: Deeshri Thapa")
+# address = input("contact address:+977 9813000000")
+# print(name)
+# print(address)
+
+# 3. Write a program which accepts personal information and displays (person id, full name, contact address).
+# id = input("enter your id:")
+# name = input("enter your name:")
+# address = input("enter your contact address:")
+# print(id)
+# print(name)
+# print(address)
+
+# 4. Write a program to print sum, difference, product, division, power of two numbers which input from the user.
+# num1 = int(input("enter first number:"))
+# num2 = int(input("enetr second number:"))
+# sum = num1 + num2
+# dif = num1 - num2
+# product = num1 * num2
+# div = num1/ num2
+# power = num1 ** num2
+# print("sum:", sum)
+# print("difference:", dif)
+# print("prodeuct:", product)
+# print("division:", div)
+# print("power:", power)
+
+# 5. Write a program to convert temperatures to and from celsius, fahrenheit.
+# fahrenheit = 54
+# celsius = ((fahrenheit-32)*5)/9
+# print("The temperature in celsius is :", celsius)
+
+# 6.  Write a program to guess a number between 1 to 9.
+# import random
+# print(random.randint(1,9))
+# print(random.random())
+
+# 7. Write a program that accepts a word from the user and reverse it.
+
+
+# 8. Write a program to calculate simple interest.
+# principal = 20
+# rate = 35
+# time =  40
+# interest= (principal*rate*time)/100
+# print("simple interest is :", interest)
+
+# 9. Write a program to calculate compound interest.
+# principle = 35
+# rate= 15
+# time = 20
+# Amount = principle * (pow((1 + rate / 100), time))
+# CI = Amount - principle
+# print("Compound interest is", CI)
+
+# 10. Write a program to check Armstrong Number.
+
+
